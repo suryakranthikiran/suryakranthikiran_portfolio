ff9b8640eaac15a29557a4e8c71715ebe664d11d
Surya kranthi kiran <tskranthikiran@gmail.com>
2025-04-28 19:48:55 +0530

    Update NavbarLogo component to enhance title formatting and add educational details

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -2,9 +2,12 @@ const NavbarLogo = () => {
   return (
     <div>
       <h1 className="text-white  text-2xl sm:hidden md:block">
-        Surya Kranthi Kiran
+        Surya Kranthi Kiran <br />{" "}
+        <span className="text-sm">
+          B.Tech-Computer Science & Engineering(2017-21)
+        </span>
       </h1>
-      <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
+      <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block sm:text-2xl">
         Kranthi
       </h1>
     </div>
